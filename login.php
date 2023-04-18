e726988eb5769a9570f1370ca66a51b8a5a78b75
ellrussbest <ellrussbest@gmail.com>
2023-04-18 21:33:06 +0200

    formatting changes

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,18 +14,19 @@
     <title>Welcome to Bestflix</title>
     <link rel="stylesheet" type="text/css" href="./assets/style/style.css" />
 </head>
+
 <body>
     <div class="signInContainer">
         <div class="column">
             <div class="header">
-                <img src="./assets/images/logo.png" title="Logo" alt="Site Logo"/>
+                <img src="./assets/images/logo.png" title="Logo" alt="Site Logo" />
                 <h3>Sign In</h3>
                 <span>to continue to Bestflix </span>
             </div>
             <form method="POST">
-                <input type="text" name="username" placeholder="Username" required/>
-                <input type="password" name="password" placeholder="Password" required/>
-                <input type="submit" name="submitButton" value="SUBMIT"/>
+                <input type="text" name="username" placeholder="Username" required />
+                <input type="password" name="password" placeholder="Password" required />
+                <input type="submit" name="submitButton" value="SUBMIT" />
             </form>
 
             <a href="register.php" class="signInMessage">
@@ -34,4 +36,5 @@
 
     </div>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -6,6 +6,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,23 +14,24 @@
     <title>Welcome to Bestflix</title>
     <link rel="stylesheet" type="text/css" href="./assets/style/style.css" />
 </head>
+
 <body>
     <div class="signInContainer">
         <div class="column">
             <div class="header">
-                <img src="./assets/images/logo.png" title="Logo" alt="Site Logo"/>
+                <img src="./assets/images/logo.png" title="Logo" alt="Site Logo" />
                 <h3>Sign Up</h3>
                 <span>to continue to Bestflix </span>
             </div>
             <form method="POST">
-                <input type="text" name="firstName" placeholder="First name" required/>
-                <input type="text" name="lastName" placeholder="Last name" required/>
-                <input type="text" name="username" placeholder="Username" required/>
-                <input type="email" name="email" placeholder="Email" required/>
-                <input type="email" name="email2" placeholder="Confirm email" required/>
-                <input type="password" name="password" placeholder="Password" required/>
-                <input type="password" name="password2" placeholder="Confirm password" required/>
-                <input type="submit" name="submitButton" value="SUBMIT"/>
+                <input type="text" name="firstName" placeholder="First name" required />
+                <input type="text" name="lastName" placeholder="Last name" required />
+                <input type="text" name="username" placeholder="Username" required />
+                <input type="email" name="email" placeholder="Email" required />
+                <input type="email" name="email2" placeholder="Confirm email" required />
+                <input type="password" name="password" placeholder="Password" required />
+                <input type="password" name="password2" placeholder="Confirm password" required />
+                <input type="submit" name="submitButton" value="SUBMIT" />
             </form>
 
             <a href="login.php" class="signInMessage">
@@ -39,4 +41,5 @@
 
     </div>
 </body>
+
 </html>
