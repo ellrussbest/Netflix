86b438fcb916a46f3a9fb83d530747d897e4dcb2
ellrussbest <ellrussbest@gmail.com>
2023-05-14 12:27:00 +0200

    finished the video part

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,11 @@ function volumeToggle(button) {
 }
 
 function previewEnded() {
-    // toggle is used to toggle either hidden or show values of the element
-    // if the element is on hidden, it will be toggled to show
-    // and if it is on show then it will be toggled to hidden
-    $(".previewVideo").toggle();
-    $(".previewImage").toggle();
+  // toggle is used to toggle either hidden or show values of the element
+  // if the element is on hidden, it will be toggled to show
+  // and if it is on show then it will be toggled to hidden
+  $(".previewVideo").toggle();
+  $(".previewImage").toggle();
 }
 
 function goBack() {
@@ -26,20 +26,92 @@ function startHideTimer() {
 
     timeout = setTimeout(() => {
       $(".watchNav").fadeOut();
-    }, 2000)
-
-  })
+    }, 2000);
+  });
 }
 
 function initVideo(videoId, username) {
   startHideTimer();
+  setStartTime(videoId, username);
   updateProgressTimer(videoId, username);
 }
 
 function updateProgressTimer(videoId, username) {
   addDuration(videoId, username);
+
+  let timer;
+
+  $("video")
+    .on("playing", (event) => {
+      window.clearInterval(timer);
+      timer = window.setInterval(() => {
+        updateProgress(videoId, username, event.target.currentTime);
+      }, 3000);
+    })
+    .on("ended", () => {
+      setFinished(videoId, username);
+      window.clearInterval(timer);
+    });
 }
 
-function addDuration() {
-  
+function addDuration(videoId, username) {
+  $.post(
+    "ajax/addDuration.php",
+    {
+      videoId,
+      username,
+    },
+    (data) => {
+      if (data !== null && data !== "") alert(data);
+    }
+  );
+}
+
+function updateProgress(videoId, username, progress) {
+  $.post(
+    "ajax/updateDuration.php",
+    {
+      videoId,
+      username,
+      progress,
+    },
+    (data) => {
+      if (data !== null && data !== "") alert(data);
+    }
+  );
+}
+
+function setFinished(videoId, username) {
+  $.post(
+    "ajax/setFinished.php",
+    {
+      videoId,
+      username,
+    },
+    (data) => {
+      if (data !== null && data !== "") alert(data);
+    }
+  );
+}
+
+function setStartTime(videoId, username) {
+  $.post(
+    "ajax/getProgress.php",
+    {
+      videoId,
+      username,
+    },
+    (data) => {
+      if (isNaN(data)) {
+        alert(data);
+        return;
+      }
+
+      $("video").on("canplay", (event) => {
+        // this.currentTime = data;
+        event.target.currentTime = data;
+        $("video").off("canplay");
+      });
+    }
+  );
 }
